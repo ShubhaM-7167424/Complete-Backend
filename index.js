01ccd2fcb3b7708e3f49e0b7d2043e632f521163
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-11-30 19:53:49 +0530

    simple authentication using cookie

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
-
+import cookieParser from "cookie-parser";
 mongoose
     .connect("mongodb://localhost:27017/", {
         dbName: "backendApp",
@@ -25,20 +25,37 @@ const app = express();
 // Middlewares
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // setting up view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index", { name: "Shubham Singh" });
+    const { token } = req.cookies;
+    if (token) {
+        res.render("logout");
+    } else {
+        res.render("login");
+    }
 });
 
-app.get("/add", async (req, res) => {
-    res.send("Nice");
+app.post("/login", (req, res) => {
+    res.cookie("token", "iamin", {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000),
+    });
+    res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+    });
+    res.redirect("/");
 });
 
 app.post("/contact", async (req, res) => {
-    const {name, email} = req.body
+    const { name, email } = req.body;
     await Messge.create({ name, email });
     res.redirect("/success");
 });
